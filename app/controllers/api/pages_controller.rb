a5f862805c864363002f4285da41f372b333b096
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-08-19 11:37:38 -0400

    create a products action

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,14 @@
 class Api::PagesController < ApplicationController
   def index
     render 'index.json.jbuilder'
+
+  def product
+    @products = Product.new(
+      name: "test",
+      price: "4",
+      description: "test test test"
+      )
+    @product.save
+    render "show.json.jbuilder"
   end
 end
